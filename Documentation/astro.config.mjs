5506b1f4ab8b8b8dc75a7dd6e8beaf090df5bbc5
V <61495413+kelo221@users.noreply.github.com>
2024-10-31 12:49:07 +0200

    Init Docs

---
+++ Documentation/astro.config.mjs
@@ -0,0 +1,43 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightOpenAPI, { openAPISidebarGroups } from 'starlight-openapi';
+
+export default defineConfig({
+    site: 'https://kelo221.github.io',
+    base: '/fs19_CSharp_Teamwork',
+    integrations: [
+        starlight({
+            title: 'My Docs',
+            social: {
+                github: 'https://github.com/withastro/starlight',
+            },
+            plugins: [
+                starlightOpenAPI([
+                    {
+                        base: 'api',
+                        label: 'My API',
+                        schema: './src/content/schemas/swagger.yaml',
+                        slugPrefix: 'api/operations/',
+                        operationIdMap: {
+                            'boards.{board_id}.lists-post': 'create-board-list',
+                            'lists.{list_id}.cards-post': 'create-list-card',
+                            'boards.{board_id}.members-post': 'add-board-member'
+                        },
+                        // Configure how operation IDs are generated
+                        operationIds: {
+                            clean: (operationId) => {
+                                return operationId
+                                    .replace(/\./g, '-')
+                                    .replace(/{|}/g, '')
+                                    .toLowerCase();
+                            }
+                        }
+                    },
+                ]),
+            ],
+            sidebar: [
+                ...openAPISidebarGroups,
+            ],
+        }),
+    ],
+});
